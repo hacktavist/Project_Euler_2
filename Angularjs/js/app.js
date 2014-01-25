ede9ffd34ad476586a5e563dcd651048e8dea733
Nic Patterson <virtuosorock39@gmail.com>
2014-01-25 17:06:54 -0500

    added angular project for the solution

---
+++ Angularjs/js/app.js
@@ -0,0 +1,28 @@
+//app.js
+var fibonacci = angular.module('fibonacci', []);
+
+fibonacci.controller("find_fib", find_fib);
+
+function find_fib($scope) {
+    var next = 1,
+        last = 0,
+        temp = 0;
+    $scope.find_fib_num = function() {
+        $scope.max;
+        $scope.fib = 0;
+        $scope.fib_seq = new Array();
+        while (last < $scope.max) {
+            temp = next + last;
+            $scope.fib_seq.push(last);
+            console.log($scope.fib_seq);
+            if (last % 2 === 0 && last < $scope.max) {
+                $scope.fib += last;
+
+            }
+            next = last;
+            last = temp;
+
+        }
+    }
+
+}
